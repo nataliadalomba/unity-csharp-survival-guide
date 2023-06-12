acc19e277987f1f9e0519406da96cfe65662a284
Natalia DaLomba <nataliadalomba@protonmail.com>
2023-06-12 10:08:22 -0400

    Starting practical example of for each loops

--- Assets/Scripts/Arrays/ArraysAndLoops.cs
+++ Assets/Scripts/Arrays/ArraysAndLoops.cs
@@ -2,4 +2,34 @@ using UnityEngine;
 
 public class ArraysAndLoops : MonoBehaviour {
 
+    [System.Serializable]
+    public class Item {
+        public int iD;
+        public string name;
+        public string description;
+    }
+
+    [SerializeField] public Item[] items;
+
+    private void Update() {
+
+    }
+
+    void ForEachLoops1() {
+
+        string[] kittenColors = { "blue", "pink", "brown", "white" };
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            foreach (string color in kittenColors)
+                if (color == "brown")
+                    Debug.Log(color);
+        }
+
+        object[] mixedArray = { 3.14, "green", 10, true };
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            foreach (var item in mixedArray)
+                Debug.Log(item);
+        }
+    }
 }
